f0d2aa46a845879fc5ddbc69bc7312eb529896a1
luamix550 <luizalfonso611@gmail.com>
2024-09-20 19:04:06 -0500

    Update server configuration

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log("hello")
+import express from "express";
+import morgan from "morgan";
+import authRoutes from "./routes/auth.routes.js";
+import contactsRouter from "./routes/contact.routes.js"
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use("/api", authRoutes);
+app.use("/api", contactsRouter)
+
+export default app;
